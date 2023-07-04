5cd1d046cf401edc75c6a2cc369b0529e092b875
Constance Weightman <constance.weightman@gmail.com>
2023-07-03 22:27:04 -0700

    7-3-23

---
+++ javascript/8kyu/squareArgument.js
@@ -0,0 +1,9 @@
+// Now you have to write a function that takes an argument and returns the square of it.
+
+function square(x) {
+    return Math.pow(x, 2);
+}
+
+console.log(square(2));
+console.log(square(3));
+console.log(square(4));
